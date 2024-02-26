8ccf4f6115cba0579bde4fa269a917701ecdcccc
zeynepvrl <zeynebvural450@gmail.com>
2024-02-26 15:18:47 +0300

    003

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import conn from "./db.js";
 import dotenv from "dotenv";
 import pageRoute from "./routes/pageRoute.js"
 import userRoute from "./routes/userRoute.js"
+import cors from "cors"
 
 
 dotenv.config();
@@ -10,6 +11,8 @@ conn();
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors({origin:'http://192.168.144.62'})) 
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
